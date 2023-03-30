5f76e05df10fc2279aeb43ecfa5b0a5ab2954e97
easybarezi20 <115116859+easybarezi20@users.noreply.github.com>
2023-03-30 15:10:58 -0700

    practice view paths

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = 'This works!'
+    elif month == "february":
+        challenge_text = 'Challenges for February!'
+    else:
+        return HttpResponseNotFound("this month is not supported!!!")
+    return HttpResponse(challenge_text)
