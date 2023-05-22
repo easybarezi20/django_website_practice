90d56a61a83fbacca5f24dd3c9a96d3f3d47a4b9
easybarezi20 <easybarezi20@gmail.com>
2023-05-21 21:16:44 -0700

    test

--- challenges/views.py
+++ challenges/views.py
@@ -20,6 +20,10 @@ monthly_challenges = {
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
+    
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
     redirect_month = months[month - 1]
     return HttpResponseRedirect("/challenges/" + redirect_month)
 
